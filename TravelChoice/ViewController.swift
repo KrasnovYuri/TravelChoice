b25c842065dcc539f5af227a06ac4dd69dc5f533
Yuri <krasnov1yuri@gmail.com>
2023-02-26 19:09:32 +0300

    Initial Commit

---
+++ TravelChoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelChoice
+//
+//  Created by Юрий Краснов on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
